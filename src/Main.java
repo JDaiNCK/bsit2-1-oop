3e9637029c91012ee0034b38888e97959b05c688
Josh Daniel I. Uy <juy62610@liceo.edu.ph>
2025-08-07 10:21:57 +0800

    Exam = Student Calculator

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
-    public static void main (String[] args) {
-        System.out.println("Main Branch");
+    public static void main(String[] args) {
+        Student s1 = new Student("Asa Mitaka", 20, "BSIT", 85.0, 90.0, 88.0);
+        Student s2 = new Student("Denji", 19, "BSIT", 85.0, 75.0, 75.0);
+        Student s3 = new Student("Aki Hayakawa", 21, "BSIT", 95.0, 85.0, 95.0);
+
+        Student[] students = {s1, s2, s3};
+        int passing = 0;
+
+        for (Student stdnt : students) {
+            stdnt.displayInfo();
+            if (stdnt.isPassing()) {
+                passing++;
+            }
+        }
+
+        System.out.println("Overall: " + passing + " out of " +
+                students.length + " students are passing. Thank you.");
     }
 }
